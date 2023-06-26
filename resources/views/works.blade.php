9c2b3532b60414a58739beb9753fe2f05c73d1f2
venera <kosarevaveneraa@mail.ru>
2023-06-26 18:46:10 +0300

    страница "как это работает"

--- resources/views/works.blade.php
+++ resources/views/works.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
     <main class="main">
-        <h1 class="hidden">Главная страница сайта НЧПК Библиотека</h1>
+        <h1 class="hidden">Дестяки доступных книг для бронирования</h1>
         <section class="promo element_pad">
             <div class="container">
                 <div class="promo_row">
@@ -14,35 +14,13 @@
                         <div class="promo_col_info_ctn">
                             <p class="badge">Все твои любимые книги уже здесь</p>
                             <div class="promo_animation_ctn">
-                                <p class="promo_heading"><span class="purple">Чтение</span> это увлекательно</p>
-                                <p class="promo_desc">удовольствие от чтения может помочь человеку в профилактике
-                                    душевного и физического здоровья</p>
+                                <p class="promo_heading"><span class="purple">Забронируй</span> прямо сейчас</p>
                                 <a href="{{route('catalog')}}" class="primary_btn">Выбрать книгу</a>
                             </div>
                         </div>
                     </div>
                     <div class="promo_col promo_bg">
-                        <div class="promo_bg_ctn">
-                            <div class="animation_book_fly">
-                                <img src="{{asset('storage/promo_book.png')}}" width="783" height="784"
-                                     alt="летающая книжка">
-                            </div>
-                        </div>
-                        <div class="promo_bg_ctn">
-                            <img src="{{asset('storage/promo_right_book.png')}}" alt="книга справа" width="783" height="784">
-                        </div>
-                        <div class="promo_bg_ctn">
-                            <img src="{{asset('storage/promo_orange_line.png')}}" alt="оранжевая линия">
-                        </div>
-                        <div class="promo_bg_ctn">
-                            <img src="{{asset('storage/promo_ellipse_lines.png')}}" alt="круг из линий">
-                        </div>
-                        <div class="promo_bg_ctn">
-                            <img src="{{asset('storage/promo_purple_line.png')}}" alt="фиолетовая линия">
-                        </div>
-                        <div class="promo_bg_ctn">
-                            <img src="{{asset('storage/promo_ellipse.png')}}" alt="круг фиолетовый" width="680" height="680">
-                        </div>
+                        <img src="{{asset('storage/works_bg.png')}}" width="75%" alt="Девушка">
                     </div>
                 </div>
             </div>
@@ -51,72 +29,37 @@
 
         <section class="choose element_pad">
             <div class="container">
-                <h2 class="main_title">Выбирайте нас</h2>
-                <div class="desc_aft_title">Наш интернет-сервис имеет множество преимуществ</div>
-                <ul class="choose_list">
-                    <li class="choose_item_1 fadeLeft">
-                        <div class="choose_item_work1">
-
-                            <div class="choose_item_row">
-                                <div class="choose_item_col choose_item_img">
-
-                                </div>
-                                <div class="choose_item_col choose_item_info">
-                                    <p class="text_work">Вы заходите на наш сайт и регистрируетесь</p>
-                                </div>
-                            </div>
+                <h2 class="main_title">Как это работает?</h2>
+                <div class="desc_aft_title">Вы можете забронировать книгу в пару кликов</div>
+                <ul class="steps_list">
+                    <li class="category_item fadeRight animateFade">
+                        <div class="choose_item_work">
+                            <p class="text_container">Вы заходите на наш сайт и регистрируетесь</p>
+                            <img src="{{asset('storage/choose_work1.png')}}" alt="Шаг1">
                         </div>
                     </li>
-                    <li class="choose_item_2 fadeLeft">
-                        <div class="choose_item_work2">
-
-                            <div class="choose_item_row">
-                                <div class="choose_item_col choose_item_img">
-
-                                </div>
-                                <div class="choose_item_col choose_item_info">
-                                    <p class="text_work">Выбираете нужную Вам книгу из каталога библиотеки</p>
-                                </div>
-                            </div>
+                    <li class="category_item fadeRight animateFade">
+                        <div class="choose_item_work">
+                            <p class="text_container">Выбираете нужную Вам книгу из каталога библиотеки</p>
+                            <img src="{{asset('storage/choose_work2.png')}}" alt="Шаг1">
                         </div>
                     </li>
-                    <li class="choose_item_3 fadeBottom">
-                        <div class="choose_item_work3">
-
-                            <div class="choose_item_row">
-                                <div class="choose_item_col choose_item_img">
-
-                                </div>
-                                <div class="choose_item_col choose_item_info">
-                                    <p class="text_work_big">Если книга доступна, то можете забронировать ее на определенное время, указав дату и время, когда Вы сможете забрать книгу в библиотеке</p>
-                                </div>
-                            </div>
+                    <li class="category_item fadeLeft">
+                        <div class="choose_item_work">
+                            <p class="text_container">Если книга доступна, то можете забронировать ее на определенное время, указав дату и время, когда Вы сможете забрать книгу в библиотеке</p>
+                            <img src="{{asset('storage/choose_work3.png')}}" alt="Шаг1">
                         </div>
                     </li>
-                    <li class="choose_item_4 fadeLeft">
-                        <div class="choose_item_work4">
-
-                            <div class="choose_item_row">
-                                <div class="choose_item_col choose_item_img">
-
-                                </div>
-                                <div class="choose_item_col choose_item_info">
-                                    <p class="text_work">Вы заходите на наш сайт и регистрируетесь</p>
-                                </div>
-                            </div>
+                    <li class="category_item fadeBottom AnimateFade">
+                        <div class="choose_item_work">
+                            <p class="text_container">Система подтверждает бронирование и отправляет Вам уведомление о том, что запрос был успешно обработан</p>
+                            <img src="{{asset('storage/choose_work4.png')}}" alt="Шаг1">
                         </div>
                     </li>
-                    <li class="choose_item_5 fadeLeft">
-                        <div class="choose_item_work5">
-
-                            <div class="choose_item_row">
-                                <div class="choose_item_col choose_item_img">
-
-                                </div>
-                                <div class="choose_item_col choose_item_info">
-                                    <p class="text_work">Выбираете нужную Вам книгу из каталога библиотеки</p>
-                                </div>
-                            </div>
+                    <li class="category_item fadeBottom AnimateFade">
+                        <div class="choose_item_work">
+                            <p class="text_container">Вы приходите в библиотеку в назначенное время и забираете забронированную книгу, предъявив свой читательский билет и подтверждение бронирования</p>
+                            <img src="{{asset('storage/choose_work5.png')}}" alt="Шаг1">
                         </div>
                     </li>
                 </ul>
